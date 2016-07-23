7bbe246b57dec9c4255675b1977f9499e4feddad
monsyc <silva.castro.mony@gmail.com>
2016-07-23 10:37:00 -0700

    commented/ added button
    added button at the top: ingresar trabajos libres
    commented: directorio
    changed address for php mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'silva.castro.mony@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'smctg10@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "SMCTG CORREO:  $name";
 $email_body = "Tienes un correo de la página de SMCTG.\n\n"."Aquí están los detalles:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
